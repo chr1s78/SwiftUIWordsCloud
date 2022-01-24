2b6ed1e77dde778f1c473c4783c8722fa1e19379
chr1s78 <ayahirano2014@126.com>
2022-01-24 12:14:42 +0800

    Initial Commit

---
+++ SwiftUIWordsCloud/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIWordsCloud
+//
+//  Created by Chr1s on 2022/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIWordsCloud/SwiftUIWordsCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWordsCloudApp.swift
+//  SwiftUIWordsCloud
+//
+//  Created by Chr1s on 2022/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWordsCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
